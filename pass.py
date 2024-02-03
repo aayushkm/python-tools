72442fdf5c1e70a3ff30f92a8cd1d915ceb802d1
aayushkm <118110854+aayushkm@users.noreply.github.com>
2024-02-03 21:55:43 +0530

    Create pass.py

---
+++ pass.py
@@ -0,0 +1,17 @@
+//Cewl
+import random
+from random import randint
+import sys
+import re
+
+data = sys.stdin.readlines()
+list = ["!",".","*",".","*","&"]
+
+
+for row in data:
+    num = randint(1,999)
+    num = str(num)
+    ran = random.choice(list)
+    row = re.sub("\n", "", row)
+    new_word = ran+row+num
+    print(new_word[1:]+new_word[0])
